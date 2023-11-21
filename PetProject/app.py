3e30c8faba3e0b6f9b21de7266f34712a56330e3
Alexandra Perez <77771061+pereza0548@users.noreply.github.com>
2023-11-21 08:26:18 -0500

    Final Upload

--- PetProject/app.py
+++ PetProject/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 import requests
-from api_keys import CALL_URL, access_token
-
+from api_keys import CALL_URL, access_token, typeURL
+import re
 
 app = Flask(__name__)
 
+# [1]
+# DEFINE A FUNCTION  TO BE CALLED LATER
+# TO MAKE THE API REQUEST AND STORE THE
+# RESPONSE IN A VARIABLE NAMED "data"
 
 def getAnimalResponse(params, message):   
     headers = {
@@ -18,53 +22,23 @@ def getAnimalResponse(params, message):
        return message
     return data
 
-@app.route('/', methods=['GET', 'POST'])
-def getParams():
-    if request.method == 'POST':
-        zipcode = request.form.get('zipcode')
-        type = request.form.get('type')
-        gender = request.form.get('gender')
-        age = request.form.get('age')
-        size = request.form.get('size')
-        sort = request.form.get('sort')
+# [2]
+# DEFINE A FUNCTION TO VALIDATE AND CHECK THE POSTAL CODE
+# ENTERED BY THE USER, RETURNING A MESSAGE IF ERROR!!
 
-        if len(zipcode) != 5:
-             return render_template('zipError.html')
-        
-        # store user input
-        params = {
-                "location" : zipcode,
-                "type" : type,
-                "gender" : gender,
-                "age": age,
-                "size": size,
-                "sort": sort
-            }
-        # Message to use when no pets in zipcode or 100 mile radius
-        message = "No pets found around here. Please try a different set of filters. There's a pet waiting to come home with you."
-           
-        # calls function using params and message if there are pets
-        # values will be returned if not message will be returned
-        r = getAnimalResponse(params, message)
+def valid_zip(zipcode):
+        return re.match(r'^\d{5}$', zipcode) is not None
 
-        if r == message:
-                return render_template('preview.html', value=r, params = params)
-        
-        details = storeDetails(r)
-        
-        return render_template('index.html', value = details, all = r)
-    
-    return render_template('Home.html')
-
-
-
-
+# [3]
+# DEFINE A FUNCTION TO AGGREGATE THE FILTERED RESPONSE 
+# THAT WAS BASED ON THE USER INPUT, IN A FORMAT OF A LIST 
+# CONTAINING EACH ANIMAL INFO AS A DICTIONARY IN A LIST
 def storeDetails(details):
     animal_details = []
     for animal in details:
         filter_details={
             'name': animal.get('name', 'N/A'),
-            'breeds': animal.get('breed', 'N/A'),
+            'breeds': animal.get('breeds', 'N/A'),
             'color' : animal.get('color', 'N/A'),
             'size': animal.get('size', 'N/A'),
             'gender': animal.get('gender', 'N/A'),
@@ -72,7 +46,6 @@ def storeDetails(details):
             'environment' : animal.get('environment', 'N/A'),
             'attributes' : animal.get('attributes', 'N/A'),
             'photos' : animal.get('photos', 'N/A'),
-            'breeds' : animal.get('breeds', 'N/A'),
             'description' : animal.get('description', 'N/A'),
             'url' : animal.get('url', 'N/A'),
             'contact' : animal.get('contact', 'N/A'),
@@ -81,34 +54,87 @@ def storeDetails(details):
         animal_details.append(filter_details)
     return animal_details
 
-@app.route("/Directions", methods= ['GET', 'POST'])
-def directions():
-        if request.method == 'POST':
-            return getParams()
-        return render_template("Directions.html")
+
+# [4] 
+# DEFINE THE ROUTE TO COLLECT USER INPUT THEN PROCESS IT
+@app.route('/', methods=['GET', 'POST'])
+
+# [5]
+#  GET THE PARAMETERS FROM USER INPUT
+def getParams():                
+    types = getTypes()   
+    if request.method == 'POST':
+        zipcode = request.form.get('zipcode')
+        state = request.form.get('state')
+        type = request.form.get('type')
+        gender = request.form.get('gender')
+        age = request.form.get('age')
+        size = request.form.get('size')
+        sort = request.form.get('sort')
+
+        # [6]
+        # VALIDATE THE ZIPCODE ENTERERED BY THE USER.
+        if not valid_zip(zipcode):
+            return render_template ('zipError.html', types = types)
+        
+        # [7]
+        # RESTORE THE OPTAINED PARAMETERS IN VARIABLES
+        # TO BE PASSED IN THE API CALL, IN THE PARAMS ARGUMENT.
+        params = {
+                "location" : zipcode, 
+                "location" : state,
+                "type" : type,
+                "gender" : gender,
+                "age": age,
+                "size": size,
+                "sort": sort
+            }
+        
+        # [8]
+        # DEFINE THE MESSAGE ARGUMENT TO FLAG THE 
+        # UNAVAILABILITY IN THE PROVIDED ZIPCODE.
+        message = "No pets found around here. Please try a different set of filters. There's a pet waiting to come home with you."
 
 
+        # NOW MAKE THE API REQUEST BY CALLING THE 
+        # getAnimalResponse() DEFINED EARLIER,
+        # PASSING THE PARAMS AND MESSAGE TO BE
+        # PROCCESSED WITH THE HEADER ARGUMENT.
+        # vALUE WILL BE RETURNED IF A PET WAS FOUND,
+        # OTHERWISE DISPLAY A MESSAGE.
 
-@app.route("/Gallery", methods=['GET', 'POST'])
-def gallery():
-   if request.method == 'POST':
-       return getParams()
+        # [9]
+        # STORE THE FILTERED RESPONSE IN A VARIABLE 'r'.
+        r = getAnimalResponse(params, message)
 
-   paramsStatic = {
-       "location" : 'Lebanon, KS',
-       "distance" : '',
+        if r == message:
+                return render_template('noresults.html', value=r, params = params, types = types)
+        
+        # [10]
+        # RESTORE THE FILTERED RESPONSE AS A LIST OF DICTIONARIES
+        # TO REPRESENT EACH AVAILABLE ANIMAL.
+        details = storeDetails(r)
+        
+        return render_template('index.html', value = details, all = r, types = types)
     
-   }
-   message = "The pets are shy, they will return later."
+    return render_template('Home.html', types = types)
 
-   rImg = getAnimalResponse(paramsStatic, message)
-   animal_details = storeDetails(rImg)
+# [11]
+# GET TYPES OF ANIMAL GROUPS TO SEARCH BY FROM API 
+def getTypes():
+    headers = {
+        "Authorization": "Bearer " + access_token
+    }
+    animalResponse = requests.get(typeURL, headers=headers)
+    
+    data =animalResponse.json().get('types')
    
-
-   return render_template("Gallery.html", value=animal_details)
-
+    types = []
+    for type in data:
+        types.append(type.get('name'))
+    return types
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
 
     
